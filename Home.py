76f5b54a5ddbc6b0b10c777e1a96e202f65cdeb9
M0rEe <62582432+M0rEe@users.noreply.github.com>
2021-12-13 03:05:03 +0200

    debugged issues on the SR and TTS

--- Home.py
+++ Home.py
@@ -1,34 +1,49 @@
-from posixpath import split, splitext
-import speech_recognition as sr  #importing speech recognition module 
-import pyttsx3    #importing text to speech module    
+import speech_recognition as sr  # importing speech recognition module
+import pyttsx3  # importing text to speech module
+import SendMail as sm
 
-engine = pyttsx3.init()     #intiallizing text to speech module to start synthesizing texts
-r = sr.Recognizer()            #starting SR module to be able to recognize user "Human" voice 
 
-with sr.Microphone() as source:          #using the default microphone as input device 
-    engine.say("Welcome , you can start talking to your voice assistant now ")
+def say_print(textstr):
+    engine = pyttsx3.init()  # initializing text to speech module to start synthesizing texts
+    engine.say(textstr)
     engine.runAndWait()
-    print("Welcome , you can start talking to your voice assistant now .\n")
-    audio = r.listen(source)
-
-    try:
-        text= r.recognize_google(audio)       
-        #testing 
-        comm = format(text)
-        comm = comm.split(" ")      #splitting the user voice to string ,thus making his command 
-
-        print('i heard that : {} is it correct ?'.format(text))
-        engine.say('i heard that : {} is it correct ?'.format(text))
-        engine.runAndWait()
-        """
-        if comm[0] == 'joke':
-            print('What’s the best thing about Switzerland? I don’t know, but the flag is a big plus.')
-            engine.say('What’s the best thing about Switzerland? I don’t know, but the flag is a big plus.')
-            engine.runAndWait()///
-        """
+    print(textstr + "\n")
 
 
-    except:           #handling the miss hearing of the user if he didnt talk 
-        print("I think I miss heard you a little bit")
-        engine.say('i think i miss heard you a little bit')
-        engine.runAndWait()
+def waitforaudio():
+    r = sr.Recognizer()  # starting SR module to be able to recognize user "Human" voice
+    with sr.Microphone() as source:  # using the default microphone as input device
+        audio = r.listen(source)
+        text = ""
+        try:
+            text = r.recognize_google(audio)
+            print(text)
+            # testing
+        except Exception:
+            print("I think I miss heard you a little bit.\n")  # handling the miss hearing of the user if he didn't talk
+            # print("Exception " + str(e))
+
+    return text.lower()
+
+
+# Start
+wake_up = "hey dude"
+print("Started")
+while True:
+    print("im listening.\n")
+    wake_command = waitforaudio()
+    if wake_command.count(wake_up) > 0:
+        say_print("Welcome , you can start talking to your voice assistant now.")
+        textcommand = waitforaudio()
+        mail_str = ["send an email", "send email", "send mail", "send a mail"]
+        weather_str = ["what about the weather", "how is the weather", "what is the forecast"]
+        for phrase in mail_str:
+            if phrase in textcommand:
+                say_print("to whom you want me to sent this email ?,i prefer to write down the email you want\n")
+                to = input()
+                say_print("what do you want to be the subject of this email ?")
+                subject = waitforaudio()
+                say_print("what do you want me to write in this email ?")
+                body = waitforaudio()
+                auth = sm.send_mail_to(to, subject, body)
+                say_print(auth)
---
+++ SendMail.py
@@ -0,0 +1,24 @@
+import smtplib
+from email.message import EmailMessage
+
+
+def send_mail_to(to_email,subject,body):
+    EMAILADDRESS = "testvoiceassiatant@gmail.com"
+    PASSWORD = "123456789test"
+    msg = EmailMessage()
+    msg['Subject'] = subject
+    msg['From'] = EMAILADDRESS
+    msg['To'] = to_email
+    msg.set_content(body)
+
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    try:
+        server.login(EMAILADDRESS, PASSWORD)
+        print("logged in ...")
+        server.send_message(msg)
+        print(f"email sent to {msg['To']}")
+        return f"email sent to {msg['To']}"
+    except smtplib.SMTPAuthenticationError:
+        print("Error logging in")
+        return "Error logging in please try again later "
